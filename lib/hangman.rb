fec1c001d9f8679ae29c9824beab0659864c976d
Issac Davis <trksprkz@gmail.com>
2022-10-25 22:44:00 -0700

    initial commity

---
+++ lib/hangman.rb
@@ -0,0 +1,5 @@
+# Create a hangman game
+  # When new game started, select random word between 5 and 12 characters long
+  # set it to secret word
+  # After guessing, displays length of word as _____ (underscores)
+  # Revealing each correct letter guessed
