20c8d8c5c6b467e56abfbabff065b5276a7c39a1
Issac Davis <trksprkz@gmail.com>
2022-10-27 00:40:16 -0700

    added various methods vital to game functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -16,25 +16,48 @@ require 'erb'
 
     def words_list
       words = File.readlines('google-10000-english-no-swears.txt')
-      word_list = []
+      $word_list = []
        words.each do |line|
-        word_list.push
+        $word_list.push
        end
     end
 
-    chosen_word = words_list.sample
+      $chosen_word = words_list.sample
+
+    def play_round
+      characters = $chosen_word.scan()
+      replacement_char = []
+        until replacement_char === characters
+          replacement_char.push['_']
+        puts replacement_char
+        end
+      guess = gets.chomp.downcase
+    end
 
     def play
       puts 'Would you like to play a round?'
        answer = gets.chomp.downcase
 
-        if answer === 'yes'
+          if answer === 'yes'
+            play_round()
+          elsif answer === 'no'
+            puts "No worries, type yes when you're ready"
+              ready_to_play = gets.chomp.upcase
+              play()
 
-        elsif answer === 'no'
-
-        elsif answer != 'yes' || answer != 'no'
-          puts 'Respond again with yes or no!'
-          play()
-        end
+          elsif answer != 'yes' || answer != 'no'
+            puts 'Respond again with yes or no!'
+            play()
+          end
       end
     play()
+
+
+    def if_accurate
+      if guess.include?(characters)
+        replacement_char = characters.gsub(Guess)
+
+      elsif Guess.include?(character) == false
+        play_round
+    end
+  end
